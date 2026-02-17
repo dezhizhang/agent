11480a9e9599f234798b55dcace2b13d8f2c9485
dezhizhang <1541609448@qq.com>
2026-02-17 22:58:14 +0800

    feat: 添加缓存文件

--- agent-api/test/internal/handler/test_app_handler.py
+++ agent-api/test/internal/handler/test_app_handler.py
@@ -1,13 +1,15 @@
+import pytest
 from pkg.response import HttpCode
 
 
 class TestAppHandler:
     """app控制器的测试类"""
 
-
-
-    def test_completion(self, client):
+    @pytest.mark.parametrize("query",[None,"你好，你是谁"])
+    def test_completion(self,query, client):
         resp = client.post("/app/completion", json={"query": "你好，你是?"})
         assert resp.status_code == 200
-        assert resp.json.get("code") == HttpCode.SUCCESS
-        print("响应内容:",resp.json)
+        if query is None:
+            assert resp.json.get("code") == HttpCode.VALIDATION_ERROR
+        else:
+            assert resp.json.get("code") == HttpCode.SUCCESS
