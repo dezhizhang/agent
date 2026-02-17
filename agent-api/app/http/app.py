b45050c2f623ae7d1ff48487b23cf3683de68fa0
dezhizhang <1541609448@qq.com>
2026-02-17 18:52:54 +0800

    feat: 添加请求验证

--- agent-api/app/http/app.py
+++ agent-api/app/http/app.py
@@ -1,7 +1,7 @@
 from injector import Injector
 from internal.router import Router
 from internal.server import Http
-
+from config import Config
 import dotenv
 
 # 将.env加载环境变量中
@@ -9,9 +9,10 @@ dotenv.load_dotenv()
 
 injector = Injector()
 
+conf = Config()
 
-app = Http(__name__,router=injector.get(Router))
 
+app = Http(__name__, config=conf, router=injector.get(Router))
 
 if __name__ == '__main__':
     app.run(debug=True)
