7549d4cbca813c21deed953c76953a3bf46c0bd9
dezhizhang <1541609448@qq.com>
2026-02-18 08:37:53 +0800

    feat:添加文件配置项

--- agent-api/config/config.py
+++ agent-api/config/config.py
@@ -17,10 +17,11 @@ def _get_bool_env(key:str) -> bool:
 class Config:
     def __init__(self):
         """关闭wtf的csrf保护"""
-        self.SECRET_KEY = os.getenv("SECRET_KEY")
         self.WTF_CSRF_ENABLED = False
+        self.SECRET_KEY = _get_env("SECRET_KEY")
         self.SQLALCHEMY_ENGINE_OPTIONS={
-            "pool_size": 30,
-            "pool_recycle": 3600,
+            "pool_size": _get_env("SQLALCHEMY_POOL_SIZE"),
+            "pool_recycle": _get_env("SQLALCHEMY_POOL_RECYCLE"),
         }
-        self.SQLALCHEMY_ECHO = True
+        self.SQLALCHEMY_ECHO = _get_bool_env("SQLALCHEMY_ECHO")
+        self.SQLALCHEMY_DATABASE_URI = _get_env("SQLALCHEMY_DATABASE_URI")
