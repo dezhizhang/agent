67b34e0f0e8899d37d6eb3481c23f01d6a0425f7
dezhizhang <1541609448@qq.com>
2026-02-18 08:32:50 +0800

    feat:从环境变量中获取值

--- agent-api/config/config.py
+++ agent-api/config/config.py
@@ -1,7 +1,26 @@
 
 import os
+from typing import Any
+from .defalt_config import DEFAULT_CONFIG
+
+def _get_env(key:str) -> Any:
+    """从环境变量中获取配置项，如果找不到则返回默认值"""
+    return os.getenv(key,DEFAULT_CONFIG.get(key))
+
+def _get_bool_env(key:str) -> bool:
+    """从环境变量中获取布尔值的配置项，如果找不到则返回默认值"""
+    value :str = _get_env(key)
+    return value.lower() == "true" if value is not None else False
+
+
+
 class Config:
     def __init__(self):
         """关闭wtf的csrf保护"""
         self.SECRET_KEY = os.getenv("SECRET_KEY")
         self.WTF_CSRF_ENABLED = False
+        self.SQLALCHEMY_ENGINE_OPTIONS={
+            "pool_size": 30,
+            "pool_recycle": 3600,
+        }
+        self.SQLALCHEMY_ECHO = True
