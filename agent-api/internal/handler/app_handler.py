dda623352a327aab2d048e506c7758055dbbbd9c
dezhizhang <1541609448@qq.com>
2026-02-19 19:17:10 +0800

    feat:修改为langchain

--- agent-api/internal/handler/app_handler.py
+++ agent-api/internal/handler/app_handler.py
@@ -1,10 +1,12 @@
 import os
 from flask import request
-from openai import OpenAI
+
 from internal.schema.app_schema import CompletionReq
 from pkg.response import success_json,validate_error_json
 from internal.exception import FailException
-
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
 
 class AppHandler:
     """应用控制器"""
@@ -12,26 +14,25 @@ class AppHandler:
     def completion(self):
         """聊天接口"""
         # 1. 提取从接口中获取输入
-        query = request.json.get('query')
-        # 2. 构建openai客户端，并发起请求
-        client = OpenAI(
-            api_key=os.getenv("OPENAI_API_KEY"),
-            base_url=os.getenv("OPENAI_API_BASE"),
-        )
         req = CompletionReq()
         if not req.validate():
             return validate_error_json(req.errors)
-        # 3. 得到请求响应，然后将OpenAI的响应传给前前端
-        completion = client.chat.completions.create(
+
+        # 构建提示词模板
+        prompt = ChatPromptTemplate.from_template("{query}")
+
+        # 2. 构建openai客户端，并发起请求
+        llm = ChatOpenAI(
             model="gpt-4o",
-            messages=[
-                {"role":"system","content":"你是OpenAI开发的聊天机器人，请根据用户的输入回复对应的信息"},
-                {"role":"user","content":query},
-            ]
+            api_key=os.getenv("OPENAI_API_KEY"),
+            base_url=os.getenv("OPENAI_API_BASE"),
         )
+        # 3. 得到请求响应，然后将OpenAI的响应传递给前端
+        ai_message = llm.invoke(prompt.invoke({"query":req.query.data}))
 
-        content = completion.choices[0].message.content
-
+        parser = StrOutputParser()
+        # 解析响应内容
+        content = parser.invoke(ai_message)
 
         return success_json({"content":content})
 
