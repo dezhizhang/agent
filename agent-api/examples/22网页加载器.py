e89b672f60252bc1b0571387d6bb6d86053a8f4a
dezhizhang <1541609448@qq.com>
2026-02-22 20:30:22 +0800

    feat:网页加载器

--- agent-api/examples/22网页加载器.py
+++ agent-api/examples/22网页加载器.py
@@ -1,13 +1,8 @@
 from langchain_community.document_loaders import WebBaseLoader
 
 loader = WebBaseLoader("https://aint.top/")
-
 document = loader.load()
 
 print(document)
 print(len(document))
 print(document[0].metadata)
-
-
-
-
