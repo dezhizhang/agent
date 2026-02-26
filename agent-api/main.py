d6af11560e8c09661a85c8c604f71e7d18936736
dezhizhang <1541609448@qq.com>
2026-02-26 23:32:42 +0800

    feat:高德开发平台api集成

--- agent-api/main.py
+++ agent-api/main.py
@@ -1,26 +1,35 @@
-from typing import Type, Any
+import os
+
+import dotenv
 from langchain_core.tools import BaseTool
-from pydantic import BaseModel,Field
+from pydantic import BaseModel, Field
+from typing import Type,Any
 
-
-class MultiplyInput(BaseModel):
-    a:int = Field(description="第一个数字")
-    b:int = Field(description="第二个数字")
-
-class MultiplyTool(BaseTool):
-    """乘法计算工具"""
-    name: str = "multiply_tool"
-    description: str = "将传递的两个数字相乘后返回"
-    args_schema: Type[BaseModel] = MultiplyInput
-
-    def _run(self, *args: Any, **kwargs: Any) -> Any:
-        return kwargs["a"] * kwargs["b"]
+dotenv.load_dotenv()
 
 
 
-calculator = MultiplyTool()
 
-print("名字",calculator.name)
-print("描述",calculator.description)
-print("是否返回",calculator.return_direct)
-print("方法调用",calculator.invoke({"a":2,"b":3}))
+class GaodeWeatherArgsSchema(BaseModel):
+    city: str = Field(description="需要查询天气预报目标城市，例如：广州")
+
+
+class GaodeWeatherTool(BaseTool):
+    """根据传入的城市名查询天气"""
+    name = "gaode_weather"
+    description = "当你想查询天气或者天气相关的问题时可以使用的工具"
+    args_schema: Type[BaseModel] = GaodeWeatherArgsSchema
+
+    def _run(self,*args:Any, **kwargs:Any) -> Any:
+        """根据传入的城市名称运行调用api获取城市对应的天气预报信息"""
+        # 1. 获取高德AP秘，如果没有创建的话创抛出错误
+        gaode_api_key = os.getenv("GAODE_API_KEY")
+        if not gaode_api_key:
+            return f"高德开放平台API未配置"
+
+        #
+
+        pass
+
+
+
