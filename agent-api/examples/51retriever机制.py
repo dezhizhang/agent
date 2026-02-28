fe175c595aa0a6e7ce9307c6f98c061fc54713b5
dezhizhang <1541609448@qq.com>
2026-02-28 21:51:38 +0800

    feat:基础查询对像

--- agent-api/examples/51retriever机制.py
+++ agent-api/examples/51retriever机制.py
@@ -1,3 +1,5 @@
+
+
 import dotenv
 from langchain_openai import OpenAIEmbeddings
 from langchain_chroma import Chroma
@@ -7,23 +9,17 @@ dotenv.load_dotenv()
 embeddings = OpenAIEmbeddings(
     model="text-embedding-3-large"
 )
-
 chroma_db = Chroma(
     persist_directory="chroma_bge",
     embedding_function=embeddings,
     collection_name="shuqing",
 )
 
-query = "hello world"
-docs = chroma_db.similarity_search(query,k=2)
+retriever = chroma_db.as_retriever()
+
+docs = retriever.invoke("hello world")
 
 for doc in docs:
     print(doc.page_content)
     print("*" * 30)
-
-
-
-
-
-
 
