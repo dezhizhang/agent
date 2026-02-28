d66b3076f271304c0cf84b21aff68a3753e51690
dezhizhang <1541609448@qq.com>
2026-02-28 22:36:58 +0800

    feat:上下文查询检索器

---
+++ agent-api/examples/52多模态查询.py
@@ -0,0 +1,33 @@
+import dotenv
+from langchain_openai import OpenAIEmbeddings,ChatOpenAI
+from langchain_classic.retrievers.multi_query import MultiQueryRetriever
+from langchain_chroma import Chroma
+
+dotenv.load_dotenv()
+
+embeddings = OpenAIEmbeddings(
+    model="text-embedding-3-large",
+)
+
+chroma_db = Chroma(
+    persist_directory="chroma_bge",
+    embedding_function=embeddings,
+    collection_name="shuqing"
+)
+
+llm = ChatOpenAI(
+    model="gpt-4o",
+)
+
+retriever = MultiQueryRetriever.from_llm(llm=llm,retriever=chroma_db.as_retriever())
+
+docs = retriever.invoke("hello world")
+
+for doc in docs:
+    print(doc.page_content)
+    print("*" * 30)
+
+
+
+
+
